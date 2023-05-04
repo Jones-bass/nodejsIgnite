5010b809e6a2978a8d669900da3b1cbe101d4181
jones-bass <jonesbass.tb@gmail.com>
2023-05-04 16:00:43 -0300

    Conhecendo HTTP status

---
+++ 01-fundamentos-nodejs/src/server.js
@@ -0,0 +1,55 @@
+import http from 'node:http'
+
+// - Criar usuários
+// - Listagem usuários
+// - Edição de usuários
+// - Remoção de usuários
+
+// - HTTP
+//   - Método HTTP
+//   - URL
+
+// GET, POST, PUT, PATCH, DELETE
+
+// GET => Buscar um recurso do back-end
+// POST => Criar um recurso no back-end
+// PUT => Atualizar um recurso no back-end
+// PATCH => Atualizar uma informação específica de um recurso no back-end
+// DELETE => Deletar um recurso do back-end
+
+// GET /users => Buscando usuários no banc-end
+// POST /users => Criar um usuário no back-end
+
+// Stateful - Stateless
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+
+// HTTP Status Code
+
+const users = []
+
+const server = http.createServer((req, res) => {
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+      .setHeader('Content-type', 'application/json')
+      .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+    })
+
+    return res.writeHead(201).end()
+  }
+
+  return res.writeHead(404).end()
+})
+
+server.listen(3333, () => {
+  console.log('🚀 Server started on port 3333!')
+})

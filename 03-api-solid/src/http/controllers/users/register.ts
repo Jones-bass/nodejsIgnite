ed914c58a599218715cbaf821e18ce6f123ce8e3
jones-bass <jonesbass.tb@gmail.com>
2023-05-27 06:52:11 -0300

    Separando ainda mais meu codigo e estruturando erros

--- 03-api-solid/src/http/controllers/users/register.ts
+++ 03-api-solid/src/http/controllers/users/register.ts
@@ -1,6 +1,8 @@
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '../../../useCases/register'
+import { PrismaUsersRepository } from '../../../repositories/prisma/prismaUsersRepository'
+import { UserAlreadyExistsError } from '../../../useCases/errors/user-already-exists-error'
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
   const registerBodySchema = z.object({
@@ -12,13 +14,17 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { name, email, password } = registerBodySchema.parse(request.body)
 
   try {
-    await RegisterUseCase({
+    const usersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(usersRepository)
+
+    await registerUseCase.execute({
       email,
       name,
       password,
     })
   } catch (err) {
-    return reply.status(409).send()
+    if (err instanceof UserAlreadyExistsError)
+      return reply.status(409).send({ message: err.message })
   }
 
   return reply.status(201).send()

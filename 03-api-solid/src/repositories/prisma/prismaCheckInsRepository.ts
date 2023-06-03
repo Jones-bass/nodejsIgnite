21e79bc25c3c3e570dbf23288ac534a748342203
jones-bass <jonesbass.tb@gmail.com>
2023-06-03 10:04:08 -0300

    caso de uso historico

--- 03-api-solid/src/repositories/prisma/prismaCheckInsRepository.ts
+++ 03-api-solid/src/repositories/prisma/prismaCheckInsRepository.ts
@@ -1,29 +1,19 @@
-import { Prisma } from '@prisma/client'
+import { CheckIn, Prisma } from '@prisma/client'
 import { prisma } from '../../lib/prisma'
 import { CheckInsRepository } from '../check-ins-repository'
-import dayjs from 'dayjs'
 
 export class PrismaCheckInsRepository implements CheckInsRepository {
+  findByUserIdOnDate(userId: string, date: Date): Promise<CheckIn | null> {
+    throw new Error('Method not implemented.')
+  }
+
+  findManyByUserId(userId: string, page: number): Promise<CheckIn[]> {
+    throw new Error('Method not implemented.')
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = await prisma.checkIn.create({
       data,
-    })
-
-    return checkIn
-  }
-
-  async findByUserIdOnDate(userId: string, date: Date) {
-    const startOfTheDay = dayjs(date).startOf('date')
-    const endOfTheDay = dayjs(date).endOf('date')
-
-    const checkIn = await prisma.checkIn.findFirst({
-      where: {
-        user_id: userId,
-        created_at: {
-          gte: startOfTheDay.toDate(),
-          lte: endOfTheDay.toDate(),
-        },
-      },
     })
 
     return checkIn

a8be60da62e4b21cdda4b3ea7fb5791798ecb7b8
jones-bass <jonesbass.tb@gmail.com>
2023-06-05 17:01:43 -0300

    Rota put e delete

--- 02-api-rest Desafio/src/routes/diet.ts
+++ 02-api-rest Desafio/src/routes/diet.ts
@@ -82,4 +82,107 @@ export async function dietRoutes(app: FastifyInstance) {
       }
     },
   )
+
+  app.put(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const sessionIdSchema = z.object({
+        sessionId: z.string().uuid(),
+      })
+
+      const { sessionId } = sessionIdSchema.parse(request.cookies)
+      const user = await knex('users').where({ session_id: sessionId }).first()
+
+      if (!user) {
+        return reply.status(403).send({
+          status: 'error',
+          data: 'Session ID does not exist',
+        })
+      }
+
+      const createUserParamsSchema = z.object({
+        id: z
+          .number({
+            coerce: true,
+          })
+          .int(),
+      })
+      const { id } = createUserParamsSchema.parse(request.params)
+
+      const createUserBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        date: z.string(),
+        hours: z.string(),
+        is_diet: z.boolean().default(false),
+      })
+
+      const { name, description, date, is_diet, hours } =
+        createUserBodySchema.parse(request.body)
+
+      const [diet] = await knex('diet')
+        .update({
+          name,
+          description,
+          date,
+          is_diet,
+          hours,
+          user_id: user.id,
+        })
+        .where({
+          id,
+          user_id: user.id,
+        })
+        .returning('*')
+
+      return {
+        status: 'success',
+        data: diet,
+      }
+    },
+  )
+
+  app.delete(
+    '/:id',
+    {
+      preHandler: [checkSessionIdExists],
+    },
+    async (request, reply) => {
+      const sessionIdSchema = z.object({
+        sessionId: z.string().uuid(),
+      })
+
+      const { sessionId } = sessionIdSchema.parse(request.cookies)
+      const user = await knex('users').where({ session_id: sessionId }).first()
+
+      if (!user) {
+        return reply.status(403).send({
+          status: 'error',
+          data: 'Session ID does not exist',
+        })
+      }
+
+      const createUserParamsSchema = z.object({
+        id: z
+          .number({
+            coerce: true,
+          })
+          .int(),
+      })
+      const { id } = createUserParamsSchema.parse(request.params)
+
+      const hasDeleted = await knex('diet').delete().where({
+        id,
+        user_id: user.id,
+      })
+
+      return {
+        status: 'success',
+        data: (hasDeleted && id) || null,
+      }
+    },
+  )
 }

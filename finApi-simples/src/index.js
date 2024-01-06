0bff77caaa6095afaf6cfcf8c72f65742965d3ae
jones-bass <jonesbass.tb@gmail.com>
2024-01-06 16:00:42 -0300

    Middleware | Rota Statement

--- finApi-simples/src/index.js
+++ finApi-simples/src/index.js
@@ -7,6 +7,19 @@ app.use(express.json());
 
 const customers = [];
 
+//Middleware
+function verifyIfExistsAccountCPF(request, response, next) {
+  const { cpf } = request.headers;
+
+  const customer = customers.find((customer) => customer.cpf === cpf);
+
+  if (!customer)
+    return response.status(400).json({ error: "Customer not found" });
+
+  request.customer = customer;
+  return next();
+}
+
 app.post('/account', ( resquest, response ) => {
   const { cpf, name } = resquest.body;
 
@@ -29,6 +42,13 @@ app.post('/account', ( resquest, response ) => {
 
   return response.status(201).send();
 })
+
+
+app.get("/statement", verifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  return response.json(customer.statement);
+});
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!')
